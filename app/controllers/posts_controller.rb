56df6fa070c3e8dc6a0cdd07dcd61d50832cf91d
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-07-09 03:37:44 -0700

    Set up post MVC unmigrated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @post = Post.new(post_params)
+
+      if @post.save
+          redirect_to new_user_path
+      else
+          render :new, status: :unprocessable_entity
+      end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:description)
+  end
 end
--- db/migrate/20240708112924_create_posts.rb
+++ db/migrate/20240708112924_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-
+      t.string :title, null: false
+      t.string :description, null: false
+      t.belongs_to :user
       t.timestamps
     end
   end
